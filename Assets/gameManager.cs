7b2a9493c4007d77d5ee4cd7ed4de6043f38bcb4
bloodstream1 <aakashtyagi2303@gmail.com>
2021-07-26 21:33:18 +0530

    brackeys beginner

---
+++ Assets/gameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+
+    public void completeLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void CompleteLevel()
+    {
+        Debug.Log("you won!!");
+    }
+
+    public void EndGame()
+    {
+        if(gameHasEnded == false)
+        {
+            Debug.Log("GAME OVER!!");
+            gameHasEnded = true;
+            Invoke("restart",restartDelay);
+        }
+    }
+
+    void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/scripts/plyerMument.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class plyerMument : MonoBehaviour
+{
+    public float force1;
+    public float force2;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(force2 * Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-force2 * Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<gameManager>().EndGame();
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,force1 * Time.deltaTime);
+    }
+
+}
